db68fe1d29721fc22682357284ab22cbd95c15c4
ANANDHAKUMAR18 <101105244+ANANDHAKUMAR18@users.noreply.github.com>
2025-01-26 20:48:28 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 var express = require('express');
 var app = express();
+var path = require('path');
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function (req, res) {
-    res.send('{ "response": "Hello From Thetips4you" }');
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));  // Serve the static HTML file
 });
 
 app.get('/will', function (req, res) {
-    res.send('{ "response": "Hello World" }');
+    res.sendFile(path.join(__dirname, 'public', 'will.html'));  // Serve the will.html file
 });
-app.get('/ready', function (req, res) {
-    res.send('{ "response": " Great!, It works!" }');
-});
+
 app.listen(process.env.PORT || 3000);
 module.exports = app;
